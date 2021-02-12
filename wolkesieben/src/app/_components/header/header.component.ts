8213824eb9b8b3db031c2860f1b0f3968f977df4
KlausFu <klaus.fuhrmeister@gmx.de>
2021-02-12 13:40:22 +0100

    Feature: added new offer page

--- wolkesieben/src/app/_components/header/header.component.ts
+++ wolkesieben/src/app/_components/header/header.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {User} from "../../_objects/user";
+import {User} from '../../_objects/user';
+import {AppService} from '../../_services/app.service';
 
 @Component({
   selector: 'app-header',
@@ -13,7 +14,7 @@ export class HeaderComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    this.user = JSON.parse(localStorage.getItem('appUser'));
+    this.user = JSON.parse(localStorage.getItem(AppService.LOCAL_STORAGE_KEY));
   }
 
 }
