2a91150cd77b602a2017ce516461ba85032a7ec5
KlausFu <klaus.fuhrmeister@gmx.de>
2021-02-13 22:17:41 +0100

    Added page to create new offer

--- wolkesieben/src/app/new-offer/new-offer.page.ts
+++ wolkesieben/src/app/new-offer/new-offer.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {UserService} from '../_services/user.service';
+import {User} from "../_objects/user";
+import {Offer} from "../_objects/offer";
 
 @Component({
   selector: 'app-new-offer',
@@ -8,9 +10,14 @@ import {UserService} from '../_services/user.service';
 })
 export class NewOfferPage implements OnInit {
 
+  offer: Offer;
+  allSpecies: any[];
+  allBreeds: any[];
+
   constructor(private userService: UserService) { }
 
   async ngOnInit() {
+    this.offer = new Offer();
     await this.userService.getUser();
   }
 
